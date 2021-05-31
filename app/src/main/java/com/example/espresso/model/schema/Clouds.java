c0734d1219882f390f9fb6ef64ba72e95dbaedb8
asm@dostavista.ru <wolfstonik>
2021-05-31 14:47:47 +0300

    DV-0008: Add Retrofit 2 and weather API functional.

---
+++ app/src/main/java/com/example/espresso/model/schema/Clouds.java
@@ -0,0 +1,13 @@
+package com.example.espresso.model.schema;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Clouds{
+
+	@SerializedName("all")
+	private int all;
+
+	public int getAll(){
+		return all;
+	}
+}
